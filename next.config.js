0a7c56d803b7f23966db9cbf2ffa704b4b71801b
Rajeshwar Kashyap <76556671+work-rjkashyap@users.noreply.github.com>
2024-08-12 22:08:36 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
-	output: "export",
-	reactStrictMode: true,
-	swcMinify: true,
-	experimental: {
-		images: {
-			unoptimized: true,
-		},
-	},
-	basePath: '',
-	assetPrefix: '',
+  output: "export",
+  reactStrictMode: true,
+  swcMinify: true,
+  basePath: isProd ? '/' : '',
+  assetPrefix: isProd ? '/' : '',
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
